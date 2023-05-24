ca10910d8674afe2932c168499c46e98293f066c
Moustapha Amadou Roufaï <moustapharouf@yahoo.fr>
2023-05-24 13:02:40 +0100

    Update Rwy_capacity.py

--- Rwy_capacity.py
+++ Rwy_capacity.py
@@ -2,16 +2,38 @@ import streamlit as st
 
 def main():
     st.title("ENTREE")
-    st.set_page_config(layout="wide")
+    st.sidebar.title("Paramètres")
 
-    loi_options = ["ALEATOIRE", "AUTRE_OPTION"]
-    typ_options = ["Type 1", "Type 2", "Type 3"]  # Remplacez par vos options réelles
+    st.sidebar.header("Piste")
+    plong = st.sidebar.text_input("Longueur", "3490")
+    plarg = st.sidebar.text_input("Largeur", "45")
+    plapp = st.sidebar.text_input("LApp", "4.8")
+    qfu = st.sidebar.selectbox("QFU", list(range(37)))
+    nbexit = st.sidebar.selectbox("NbExit", list(range(1, 21)))
 
-    loi = st.selectbox("LOI", loi_options)
-    typ = st.selectbox("Type", typ_options)
-    proportion = st.slider("Proportion", 0, 100)
+    st.sidebar.header("Aéronef")
+    list_aeronef = st.sidebar.selectbox("Type", ["EA33/M"])
+    melange = st.sidebar.selectbox("Proportion", list(range(101)))
 
-    # Ajoutez les autres éléments de l'interface utilisateur ici
+    st.sidebar.header("LOI")
+    loi = st.sidebar.radio("LOI", ["ALEATOIRE", "AUTRE"])
+
+    st.sidebar.header("Simulation")
+    simul_button = st.sidebar.button("Lancer la Simulation")
+
+    if simul_button:
+        st.write("Simulation en cours...")
+
+    st.write(f"Longueur : {plong}")
+    st.write(f"Largeur : {plarg}")
+    st.write(f"LApp : {plapp}")
+    st.write(f"QFU : {qfu}")
+    st.write(f"NbExit : {nbexit}")
+
+    st.write(f"Type : {list_aeronef}")
+    st.write(f"Proportion : {melange}")
+
+    st.write(f"LOI : {loi}")
 
 if __name__ == "__main__":
     main()
