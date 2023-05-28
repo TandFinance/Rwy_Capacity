d0a9b77b3307d81f33335c00f4537d3993055c09
Moustapha Amadou Roufaï <moustapharouf@yahoo.fr>
2023-05-28 14:51:18 +0200

    Update Rwy_capacity2.py

--- Rwy_capacity2.py
+++ Rwy_capacity2.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
-selected_aeronefs=[]
 if "selected_aeronefs" not in st.session_state:
+        selected_aeronefs=[]
         st.session_state.selected_aeronefs = []
 def load_aeronef():
     df = pd.read_csv("aeronef.csv")
